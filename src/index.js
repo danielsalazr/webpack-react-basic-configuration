9e84b14c8cf548da4aa558bece473f3a0c6be888
sebastian <sebastian.narvaez@johannaortiz.com>
2023-02-13 16:22:00 -0500

    Entorno configurado para iniciar proyecto con react 18, bootstrap, SASS y  webpack

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import ReactDom from 'react-dom';
+// import ReactDom from 'react-dom';
+import ReactDom from 'react-dom/client';
 import App from './components/App';
 import './styles/global.scss';
+import './styles/bootstrap.min.css';
 
-
-ReactDom.render(<App />, document.getElementById('app'));
+ReactDom.createRoot (document.getElementById('app')).render(<App />, );
